384b1445d1ab6e22dd27c0af80160fbfb1e26e1d
MeliseAne <54532964+MeliseAne@users.noreply.github.com>
2022-10-13 19:11:24 +0300

    initial

---
+++ src/Technologies.js
@@ -0,0 +1,11 @@
+function Technologies() {
+    return (
+        <ul>
+            <ol>css</ol>
+            <ol>js</ol>
+            <ol>html</ol>
+        </ul>
+    );
+}
+
+export default Technologies;
---
+++ src/Test.js
@@ -0,0 +1,10 @@
+function Test() {
+    return (
+        <ul>
+            <ol>123</ol>
+            <ol>432</ol>
+            <ol>432</ol>
+        </ul>
+    );
+}
+export default Test;
