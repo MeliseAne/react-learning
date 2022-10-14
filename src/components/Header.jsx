e75101aafacfbf58b2917a4f33c5c05dfcf42b25
MeliseAne <54532964+MeliseAne@users.noreply.github.com>
2022-10-14 14:08:51 +0300

    new css files add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-
+import s from './Header.module.css';
 
 const Header = () => {
-    return ( <header className='header'>
+    return ( <header className={s.header}>
     <img src='https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png'></img>
   </header>);
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,27 @@
-
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-    <div className='content'>
-        <div>
-            <img src='https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws='></img>
-        </div>
-        <div>
-            ava +descr
-        </div>
+        <div className='content'>
+            <div>
+                <img src='https://media.istockphoto.com/photos/wild-grass-in-the-mountains-at-sunset-picture-id1322277517?k=20&m=1322277517&s=612x612&w=0&h=ZdxT3aGDGLsOAn3mILBS6FD7ARonKRHe_EKKa-V-Hws='></img>
+            </div>
+            <div>
+                ava +descr
+            </div>
 
-        <div>
-            my posts
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div className='Posts'>
+                <div className={s.item}>post 1</div>
+                <div className={s.item}>post 2</div>
+            </div>
         </div>
-        <div>
-            New post
-        </div>
-
-        <div>p1</div>
-        <div>p2</div>
-
-    </div>);
+    );
 }
 
 export default Profile;
