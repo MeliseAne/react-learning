4ad704ccdc1a4edf37705059d2ae42c0fe9a4368
MeliseAne <54532964+MeliseAne@users.noreply.github.com>
2022-10-19 16:17:12 +0300

    change some files

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,6 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import Like from './Post/Like';
+
 
 const MyPosts = () => {
     return (
@@ -11,10 +11,8 @@ const MyPosts = () => {
             <textarea></textarea>
             <button>Add post</button>
             <div className={s.posts}>
-                <Post message = 'Hi, how are you?' />
-                <Like count = '15'/>
-                <Post message = "It's mu first post" />
-                <Like count = '20'/>
+                <Post message = 'Hi, how are you?' likeCount='22' />
+                <Post message = "It's mu first post" likeCount='20' />               
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,5 +1,5 @@
 import s from './Post.module.css';
-import Like from '../Post/Like';
+
 
 const Post = (props) => {
     //console.log (props.message);
@@ -7,7 +7,9 @@ const Post = (props) => {
         <div className={s.item}>
             <img src='https://i.pinimg.com/736x/6d/54/9e/6d549e372a8b81bb2da23089526c2d7c.jpg' />
             {props.message}
-           <Like/>
+            <div>
+                <span> like </span> {props.likeCount}
+            </div>
         </div>
 
     );
